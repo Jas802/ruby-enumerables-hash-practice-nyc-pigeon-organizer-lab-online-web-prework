e7fd194e319b52f447821a4b9cba4b343a9fd767
Jas Spencer <jas.spencer414@gmail.com>
2019-09-05 17:27:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,17 +3,17 @@ def nyc_pigeon_organizer(data)
    data.each do |key, value|
      value.each do |new_value, names|
        names.each do |name|
-         
+
          if !new_hash[name]
            new_hash[name] = {}
          end
- 
+
          if !new_hash[name][key]
            new_hash[name][key] = []
          end
- 
+
          new_hash[name][key] << new_value.to_s
- 
+
        end
      end
    end
